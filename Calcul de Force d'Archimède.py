5c5318e80bde3bbf9ee1cd3f65f7bf6bd9c35038
Angel-Gab <angele.gaboriau@isen-ouest.yncrea.fr>
2020-06-16 18:24:21 +0200

    Mardi

--- Calcul de Force d'Archimède.py
+++ Calcul de Force d'Archimède.py
@@ -29,10 +29,11 @@ class Navire :
             liste_des_vecteurs = liste_des_vecteurs[4:]
         fichier.close()
         self.__normal = liste_des_vecteurs_normaux
-        self.__facette = liste_des_facettes
+        self._facette = liste_des_facettes
+        print(self._facette)
 
     def getf(self):
-        return self.__facette
+        return self._facette
 
     def getn(self):
         return self.__normal
@@ -48,36 +49,72 @@ class Navire :
     def Norme(self, u):
         return (u[0]**2+u[1]**2+u[2]**2)**(1/2)
 
-    def Surface(self, A, B, C):
-        AB = np.array([B[0]-A[0], B[1]-A[1], B[2]-A[2]])
-        AC = np.array([C[0]-A[0], C[1]-A[1], C[2]-A[2]])
+    def Surface(self, facette): # ++ Calcul et renvoi la norme du vecteur surface d'une facette donnés en entrée
+        AB = np.array([facette[1][0]-facette[0][0], facette[1][1]-facette[0][1], facette[1][2]-facette[0][2]])
+        AC = np.array([facette[2][0]-facette[0][0], facette[2][1]-facette[0][1], facette[2][2]-facette[0][2]])
         DsFk = self.Norme(self.ProdVect(AB, AC))/2
         return DsFk
 
-    def Calculfacette(self, vecteur_normal, A, B, C):
+
+    def Calculfacette(self, vecteur_normal, facette):
         ro = 1000
         g = 9.81
-        Fkx = ro*g*np.array([(A[0]+B[0]+C[0])/3, (A[1]+B[1]+C[1])/3, (A[2]+B[2]+C[2])/3])[2]*self.Surface(A, B, C)*vecteur_normal[0]
-        Fky = ro*g*np.array([(A[0]+B[0]+C[0])/3, (A[1]+B[1]+C[1])/3, (A[2]+B[2]+C[2])/3])[2]*self.Surface(A, B, C)*vecteur_normal[1]
-        Fkz = ro*g*np.array([(A[0]+B[0]+C[0])/3, (A[1]+B[1]+C[1])/3, (A[2]+B[2]+C[2])/3])[2]*self.Surface(A, B, C)*vecteur_normal[2]
+        Fkx = ro*g*np.array([(facette[0][0]+facette[1][0]+facette[2][0])/3, (facette[0][1]+facette[1][1]+facette[2][1])/3, (facette[0][2]+facette[1][2]+facette[2][2])/3])[2]*self.Surface(facette)*vecteur_normal[0]
+        Fky = ro*g*np.array([(facette[0][0]+facette[1][0]+facette[2][0])/3, (facette[0][1]+facette[1][1]+facette[2][1])/3, (facette[0][2]+facette[1][2]+facette[2][2])/3])[2]*self.Surface(facette)*vecteur_normal[1]
+        Fkz = ro*g*np.array([(facette[0][0]+facette[1][0]+facette[2][0])/3, (facette[0][1]+facette[1][1]+facette[2][1])/3, (facette[0][2]+facette[1][2]+facette[2][2])/3])[2]*self.Surface(facette)*vecteur_normal[2]
         return [Fkx, Fky, Fkz]
 
-    def Calculfacettes(self):
+    def Calculfacettes(self,facette):
         Kx = 0
         Ky = 0
         Kz = 0
         for i in range(0, len(self.__normal)):
             #if self.__facette[i][0][2] > 0 or self.__facette[i][1][2] > 0 or self.__facette[i][2][2]:
-                Fk = self.Calculfacette(self.__normal[i], self.__facette[i][0], self.__facette[i][1], self.__facette[i][2])
-                print(Fk)
+                Fk = self.Calculfacette(self.__normal[i], facette[i])
+                #print(Fk)
                 Kx += Fk[0]
                 Ky += Fk[1]
                 Kz += Fk[2]
-        return [Kx, Ky, Kz]
+        self._archimede = [Kx, Ky, Kz]
+
+        return self._archimede
+
+
+    def Translation(self,ecart):
+        liste_facettes = self._facette
+        for i in liste_facettes :
+            for j in i :
+                j[2] += ecart
+        return liste_facettes
+
+    def Dichotomie(self,masse,a,b, epsilon):   # a modifier
+        Fp = masse * 9.81   #Norme de la force poids
+        maxi = b
+        mini = a
+        while abs(maxi-mini) > epsilon :
+            milieu = (maxi+mini)/2
+            newA = self.Translation(mini)
+            newB = self.Translation(milieu)
+            FaA = self.Calculfacettes(newA)
+            FaB = self.Calculfacettes(newB)
+            norme_FaA = (FaA[0]**2+FaA[1]**2+FaA[2]**2)**(1/2)
+            norme_FaB = (FaB[0]**2+FaB[1]**2+FaB[2]**2)**(1/2)
+            phiA = norme_FaA-Fp
+            phiM = norme_FaB-Fp
+
+            if phiA*phiM < 0:
+                maxi=milieu
+            else :
+                mini= milieu
+        return mini
+
+
 
 
 #programme principal
 #print(CalculFfacette(np.array([0, 1, 0]), np.array([0, 0, 0]), np.array([1, 0, 0]), np.array([0, 0, 1])))
 #Lire_Stl(r"V_HULL.stl")
-Bateau = Navire("Mini650_HULL.stl")
-print(Bateau.Calculfacettes())
+Bateau = Navire("Rectangular_HULL - Copie (2).stl")
+#print(Bateau.Calculfacettes())
+print(Bateau.Dichotomie(15000,-100,100, 0.2))
+#print(Bateau.Translation(2))
---
+++ Dichotomie.py
@@ -0,0 +1,20 @@
+  #Test de Dichotomie
+class D:
+
+
+
+    def phi(self,x):
+        f= 2*x+3
+        return f
+
+    def dichotomie(self,a,b,epsilon):
+            while abs(b-a) > epsilon :
+                milieu = (b+a)/2
+                if self.phi(a)*self.phi(milieu) < 0:
+                    b=milieu
+                else :
+                    a= milieu
+            return a
+
+d = D()
+print(d.dichotomie(-10,10,0.2))
