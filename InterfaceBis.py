4151ad869a8e3d822b46d9770be36f06daf739c1
Angel-Gab <angele.gaboriau@isen-ouest.yncrea.fr>
2020-06-18 18:22:55 +0200

    Jeudi

--- InterfaceBis.py
+++ InterfaceBis.py
@@ -9,11 +9,12 @@ from PySide2.QtCore import Qt
 from PySide2.QtGui import QIcon
 from Calcul import *
 
-class Interface(QWidget):
+class Interface(QWidget):     #Classe de la deuxième IHM ouverte
     def __init__(self,stl,masse,epsilon,g):
         QWidget.__init__(self)
 
         self.__stl = stl
+        self.__iseq = False   #Création d'un argument pour ne pas bouger le bateau à l'infini
 
 
         Fichier = Info_fichier_stl(stl)   #Récupération des données du fichier stl demandé
@@ -57,7 +58,7 @@ class Interface(QWidget):
         self.canvas2.draw()
         self.canvas2.setFixedSize(750,500)
 
-
+        #Layout de l'IHM
 
         self.layout = QGridLayout()
         self.setWindowTitle("Boat Sinking Interface")
@@ -84,21 +85,23 @@ class Interface(QWidget):
 
 
 
-    def buttonLoad3DClicked(self):
+    def buttonLoad3DClicked(self):    #Fontionc appliquée au "clic" du bouton
 
         # 3D
-        self.fig = plt.figure()
-        self.canvas = FigureCanvas(self.fig)
-        self.__ax.remove()
-        self.__ax = plt.axes(projection='3d')
-        self.__your_mesh.translate([0, 0, self.__equilibre])
-        self.__ax.add_collection3d(mplot3d.art3d.Poly3DCollection(self.__your_mesh.vectors))
-        scale = self.__your_mesh.points.flatten("C")
+        if self.__iseq == False :    #Permet de ne pas pouvoir retranslater le bateau une fois la position d'équilibre atteinte
+            self.fig = plt.figure()       #Une fois lancer la simulation translate le bateau à sa position d'équilibre
+            self.canvas = FigureCanvas(self.fig)
+            self.__ax.remove()
+            self.__ax = plt.axes(projection='3d')
+            self.__your_mesh.translate([0, 0, self.__equilibre])
+            self.__ax.add_collection3d(mplot3d.art3d.Poly3DCollection(self.__your_mesh.vectors))
+            scale = self.__your_mesh.points.flatten("C")
 
-        self.__ax.auto_scale_xyz(scale, scale, scale)
-        #hfont = {'fontname':'Comic Sans MS'}
-        plt.title("Représentation 3D",color = "darkcyan")
-        self.layout.addWidget(self.canvas,1,1,1,1)
+            self.__ax.auto_scale_xyz(scale, scale, scale)
+            #hfont = {'fontname':'Comic Sans MS'}
+            plt.title("Représentation 3D",color = "darkcyan")
+            self.layout.addWidget(self.canvas,1,1,1,1)
+            self.__iseq = True
 
         #2D
 
@@ -115,11 +118,11 @@ class Interface(QWidget):
 
 
 
-class Parametres(QWidget) :
+class Parametres(QWidget) :    #Classe de la première IHM
     def __init__(self):
         QWidget.__init__(self)
-        self.__isClosed =0
-        self.__erreur =1
+        self.__isClosed = False      #Argument permettant de ne pas fermer l'IHM et de ne pas ouvrir la suivante avant que les valeurs rentrées ne soient complètes
+
 
         self.setWindowTitle("Paramétrages")
         self.setFixedSize(400, 200)
@@ -137,30 +140,30 @@ class Parametres(QWidget) :
         self.__gravite=""
 
         self.label = QLabel("Entrez les paramètres")
-        self.label.setStyleSheet("QLabel { font : 13pt ; }")
+        self.label.setStyleSheet("QLabel { font : 13pt ; }")   #Changement de la taille du texte
         self.label.setAlignment(Qt.AlignCenter)
 
-        self.label1= QLabel("Fichier STL :")
+        self.label1= QLabel("Fichier STL :")        #Création d'entrées de textes
         self.label2= QLabel("Masse :")
         self.label3= QLabel("Précision :")
         self.label4 = QLabel("Gravité :")
         self.button = QPushButton("Valider")
 
-        self.deroulement = QComboBox()
-        self.deroulement.addItem("Abeille_Bourbon.stl")
+        self.deroulement = QComboBox()    #Utilisation d'un dérouleur pour sélectionner le bateau choisi
+        self.deroulement.addItem("Abeille_Bourbon.stl")     #Ajout de tous les bateaux disponibles
         self.deroulement.addItem("Rectangle.stl")
         self.deroulement.addItem("Cylindre.stl")
         self.deroulement.addItem("BargeAlu.stl")
         self.deroulement.addItem("SousMarin.stl")
         self.deroulement.addItem("Wigley.stl")
-        self.deroulement.addItem("Mer.stl")
+        #self.deroulement.addItem("Mer.stl")
 
-        self.edit2= QLineEdit()
+        self.edit2= QLineEdit()     #Permet à l'utilisateur d'entrer les valeurs désirées
         self.edit3= QLineEdit()
         self.edit4= QLineEdit()
 
 
-        self.layout.addWidget(self.label,0,0,1,2)
+        self.layout.addWidget(self.label,0,0,1,2)    #Positionnement des élèments dans le layout
         self.layout.addWidget(self.label1,1,0)
 
         self.layout.addWidget(self.label2,2,0)
@@ -173,7 +176,7 @@ class Parametres(QWidget) :
         self.layout.addWidget(self.edit4,4,1)
         self.layout.addWidget(self.button,6,0,1,2)
 
-        self.button.clicked.connect(self.buttonClicked)
+        self.button.clicked.connect(self.buttonClicked)    #Appel de la fonction qui connecte le bouton à sa fonctionnalité
 
 
         self.setLayout(self.layout)
@@ -183,6 +186,7 @@ class Parametres(QWidget) :
 
     def getSTL(self):
         return self.__stl
+
     def getEr(self):
         return self.__erreur
 
@@ -196,50 +200,38 @@ class Parametres(QWidget) :
         return self.__precision
 
 
-    def formatstl(self,stl):
-        if stl[-4:]==".stl":
-            return stl
-        else:
-            stl =stl+".stl"
-            return stl
 
     def buttonClicked(self):
 
-
-        self.__stl =self.deroulement.currentText ()
+        self.__stl =self.deroulement.currentText ()   #Récupération des informations rentrées par l'utilisateur
         self.__masse = self.edit2.text()
         self.__precision = self.edit3.text()
         self.__gravite = self.edit4.text()
 
-        if self.isEmpty(self.__stl,self.__masse,self.__precision,self.__gravite)==True:
+        if self.isEmpty(self.__stl,self.__masse,self.__precision,self.__gravite)==True:   #Vérification que les informations sont présentes
             self.label = QLabel("Vérifiez vos informations !")
             self.label.setStyleSheet("color : #F24C04;")
             self.label.setAlignment(Qt.AlignCenter)
             self.layout.addWidget(self.label, 5, 0, 1, 2)
-            self.setLayout(self.layout)
+            self.setLayout(self.layout)                                                    #Sinon, envoie un message d'erreur
             return
-        if self.__masse.isalpha()!=False or self.__precision.isalpha()!=False or self.__gravite.isalpha()!=False:
+        if self.__masse.isalpha()!=False or self.__precision.isalpha()!=False or self.__gravite.isalpha()!=False:   #Vérification que les informations sont au bon format
             self.label = QLabel("Vérifiez vos informations !")
             self.label.setStyleSheet("color : #F24C04;")
             self.label.setAlignment(Qt.AlignCenter)
             self.layout.addWidget(self.label, 5, 0, 1, 2)
-            self.setLayout(self.layout)
+            self.setLayout(self.layout)                                                                             #Sinon, envoie un message d'erreur
             return
 
 
 
-
-
-
-
-        self.__stl = self.formatstl(self.__stl)
         self.__masse = float(self.__masse)
         self.__precision = float(self.__precision)
         self.__gravite = float(self.__gravite)
         self.close()
-        self.__isClosed = 1
+        self.__isClosed = True                      #Permet de lancer la seconde IHM
 
-    def isEmpty(self,stl,masse,precision,gravite):
+    def isEmpty(self,stl,masse,precision,gravite):    #Vérification des informations entrées
         if stl=="" or masse=="" or precision=="" or gravite=="":
             return True
         else:
@@ -249,10 +241,7 @@ class Parametres(QWidget) :
 
 
 
-
-
-
-
+#Programme principal
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -261,7 +250,7 @@ if __name__ == "__main__":
     winP = Parametres()
     winP.show()
     app.exec_()
-    while winP.getClosed() == 0:
+    while winP.getClosed() == False :    #Bloque l'avancement du programme tant que la fenêtre paramètres est ouverte
         None
 
 
